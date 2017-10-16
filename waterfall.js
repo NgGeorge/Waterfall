f3405940741c52c7f8ea6c702bff591cc6dcc3c3
trignition <kenny-chung+github@berkeley.edu>
2017-10-15 18:19:05 -0700

    Trying out storage api

--- waterfall.js
+++ waterfall.js
@@ -1,6 +1,30 @@
 (function ($) {
 	var obs_count = 0; 				// Number of events observed by an observer
-	
+	var enabledState;
+
+	// TODO: function getEnabledState gets state from storage, or sets it if it doesn't yet exist; use in conjuncion with chrome.storage.onChanged.addListener...
+	function getEnabledState() {
+		chrome.storage.local.get("enabledState", function(result) {
+			if (result == null) {
+				setEnabledState(true);
+			} else {
+				enabledState = result;
+			}
+		});
+	}
+
+	// Sets enabledState in chrome local storage.
+	function setEnabledState(state) {
+		chrome.storage.local.set({
+			'enabledState': state
+		}, function (result) {
+			if (chrome.runtime.error) {
+				console.log(chrome.runtime.error);
+			}
+		});
+	}
+
+
 	// For Pages with dynamic feeds
 	// Creates an observer, and tells it what to do when it successfully observes mutations
 	var createObserver = function(item, filterDelay) {
@@ -27,8 +51,10 @@
 	}
 
     // Persists the state of the enable button (even when the popup is closed), and dynamically changes the text when the button is pressed; might use the storage API in the future.
-    const setupEnabledSwitch = function(state, port) {
-        $('#enabledLabel').text(state ? 'Enabled' : 'Enable');
+    var setupEnabledSwitch = function(state, port) {
+				// Update the button text based on the enabled state.
+				$('#enabledLabel').text(state ? 'Enabled' : 'Enable');
+
         $('#enabledSwitch').attr('checked', state ? 'checked' : null).click(function() {
             $('#enabledLabel').text($('#enabledLabel').text() == 'Enabled' ? 'Enable' : 'Enabled');
             port.postMessage({cmd: "setEnabledState", data: !state});
@@ -43,24 +69,27 @@
 
 	// Run script
 	$(function () {
-        // Get the enabled state.
-        const backgroundPort = chrome.runtime.connect({name: "background"});
+				// Only run on reddit
+				if (window.location.href.indexOf("reddit") === -1 ) return;
+
+				// Get the enabled state.
+        var backgroundPort = chrome.runtime.connect({name: "background"});
         backgroundPort.postMessage({cmd: "getEnabledState"});
         backgroundPort.onMessage.addListener(function(response) {
-            setupEnabledSwitch(response, backgroundPort);
-
+						setupEnabledSwitch(response, backgroundPort);
             // Only runs the entire extension if "enabledState" returns true
             if (!response) return;
-            
-            // Only run on reddit
-            if (window.location.href.indexOf("reddit") === -1 ) return;
-
-            // Autoscroll down page 
-            // Interval speed is in milliseconds. 
-            // Will want to hook up an adjustable speed slider 
-            // on the HTML Popup to work with this
-            let interval = 50;
-            const scroll = setInterval(function(){ window.scrollBy(0, 1); }, interval); 
         });
+
+				// TODO: Upon any change to storage, re-runs scroll code.
+				chrome.storage.onChanged.addListener(function(changes, areaName) {
+					for (key in changes) {
+						var newValue = changes[key].newValue; // can be true, or false
+						console.log(newValue);
+						// Update the enabled/disabled button text
+
+						// Stop or restart the scrolling.
+					}
+				});
 	});
 }(window.jQuery));
