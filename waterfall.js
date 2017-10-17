fe62ee9172a5bae177a32fc650cf45ae44d038a0
trignition <kenny-chung+github@berkeley.edu>
2017-10-16 17:19:24 -0700

    placed misplaced code

--- waterfall.js
+++ waterfall.js
@@ -52,55 +52,40 @@
 		});
 	}
 
-    // Persists the state of the enable button
-    // var setupEnabledSwitch = function(state, port) {
-		// 		// Update the button text based on the enabled state.
-		// 		$('#enabledLabel').text(state ? 'Enabled' : 'Enable');
-		//
-    //     $('#enabledSwitch').attr('checked', state ? 'checked' : null).click(function() {
-    //         $('#enabledLabel').text($('#enabledLabel').text() == 'Enabled' ? 'Enable' : 'Enabled');
-    //         port.postMessage({cmd: "setEnabledState", data: !state});
-    //     });
-    // }
-
-
 	// Case insensitive jquery contains
 	jQuery.expr[':'].icontains = function(element, index, match) {
 		return jQuery(element).text().toUpperCase()
 		.indexOf(match[3].toUpperCase()) >= 0;
 	};
 
-	function setAutoScroll(interval) {
-
-	}
-	// Autoscroll down page
-	// Interval speed is in milliseconds.
-	// Will want to hook up an adjustable speed slider
-	// on the HTML Popup to work with this
-	let interval = 50;
-	const scroll = setInterval(function(){
-		window.scrollBy(0, 1);
-
-		// Check if the current window is at the bottom of the page
-		if ((window.innerHeight + window.scrollY + 1) >= document.body.scrollHeight) {
-
-			// Gets the last child element of sitetable with the class of thing
-			const $lastThreadChild = $('.thing:last');
-
-			// Check if the load more comments button exists at the bottom of the page
-			if ($lastThreadChild.hasClass('morechildren')) {
-
-			 // Click the load more comments button
-			 $lastThreadChild.find('a span').click();
-			}
-		}
-	}, interval);
-
 	// Run script
 	$(function () {
 		var enabledState;
 		// Only run on Reddit; this doesn't seem to work on Kenny's end
 		if (window.location.href.indexOf("reddit") === -1 ) return;
+
+		// Autoscroll down page
+		// Interval speed is in milliseconds.
+		// Will want to hook up an adjustable speed slider
+		// on the HTML Popup to work with this
+		let interval = 50;
+		const scroll = setInterval(function(){
+			window.scrollBy(0, 1);
+
+			// Check if the current window is at the bottom of the page
+			if ((window.innerHeight + window.scrollY + 1) >= document.body.scrollHeight) {
+
+				// Gets the last child element of sitetable with the class of thing
+				const $lastThreadChild = $('.thing:last');
+
+				// Check if the load more comments button exists at the bottom of the page
+				if ($lastThreadChild.hasClass('morechildren')) {
+
+				 // Click the load more comments button
+				 $lastThreadChild.find('a span').click();
+				}
+			}
+		}, interval);
 
 		console.log('what is enabledstate?', enabledState);
 
