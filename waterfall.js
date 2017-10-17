eb235529cbc5359496b8c19a8b45ee6795619a0f
George Ng <gngcp@uw.edu>
2017-10-16 17:34:35 -0700

    Wrapping the rest of the code in the local storage callback

--- waterfall.js
+++ waterfall.js
@@ -1,19 +1,10 @@
+'use strict';
+
 (function ($) {
 	var obs_count = 0; 				// Number of events observed by an observer
 
 	var enabledState;
 
-	// Looks in storage for an object called "enabledState"
-	chrome.storage.local.get("enabledState", function(storageItems) {
-		var enabledState = storageItems;
-		// console.log("inner enabledState = ", enabledState)
-		if (enabledState["enabledState"] == null) {
-			// Set it to the default value
-			enabledState = false;
-			saveEnabledState();
-		}
-		// console.log("inner enabledState 2 = ", enabledState);
-	});
 
 	// Sets enabledState in chrome local storage.
 	function saveEnabledState() {
@@ -60,52 +51,67 @@
 
 	// Run script
 	$(function () {
-		var enabledState;
-		// Only run on Reddit; this doesn't seem to work on Kenny's end
-		if (window.location.href.indexOf("reddit") === -1 ) return;
 
-		// Autoscroll down page
-		// Interval speed is in milliseconds.
-		// Will want to hook up an adjustable speed slider
-		// on the HTML Popup to work with this
-		let interval = 50;
-		const scroll = setInterval(function(){
-			window.scrollBy(0, 1);
-
-			// Check if the current window is at the bottom of the page
-			if ((window.innerHeight + window.scrollY + 1) >= document.body.scrollHeight) {
-
-				// Gets the last child element of sitetable with the class of thing
-				const $lastThreadChild = $('.thing:last');
-
-				// Check if the load more comments button exists at the bottom of the page
-				if ($lastThreadChild.hasClass('morechildren')) {
-
-				 // Click the load more comments button
-				 $lastThreadChild.find('a span').click();
-				}
+		// Looks in storage for an object called "enabledState"
+		chrome.storage.local.get("enabledState", function(storageItems) {
+			enabledState = storageItems;
+			console.log("inner enabledState = ", enabledState)
+			if (enabledState["enabledState"] == null) {
+				// Set it to the default value
+				console.log("TEST");
+				enabledState = false;
+				saveEnabledState();
 			}
-		}, interval);
-
-		console.log('what is enabledstate?', enabledState);
 
 
+			// Only run on Reddit; this doesn't seem to work on Kenny's end
+			if (window.location.href.indexOf("reddit") === -1 ) return;
+			console.log(enabledState);
 
-		$('#enabledSwitch').click(function() {
-			console.log("clicked!");
-			$('#enabledLabel').text(enabledState ? 'Enabled' : 'Enable');
-			// Clicking the on/off button sends the opposite value to storage.
-			setEnabledState(!enabledState);
-		});
+			// Autoscroll down page
+			// Interval speed is in milliseconds.
+			// Will want to hook up an adjustable speed slider
+			// on the HTML Popup to work with this
+			let interval = 50;
+			const scroll = setInterval(function(){
+				window.scrollBy(0, 1);
 
-		// TODO: Upon any change to storage, re-runs scroll code.
-		chrome.storage.onChanged.addListener(function(changes, areaName) {
-				var newState = changes["enabledState"].newValue;
-				console.log(newState);
-				// Update the enabled/disabled button text
-				$('#enabledLabel').text(state ? 'Enabled' : 'Enable');
-        $('#enabledSwitch').attr('checked', state ? 'checked' : null);
-				// Stop or restart the scrolling.
+				// Check if the current window is at the bottom of the page
+				if ((window.innerHeight + window.scrollY + 1) >= document.body.scrollHeight) {
+
+					// Gets the last child element of sitetable with the class of thing
+					const $lastThreadChild = $('.thing:last');
+
+					// Check if the load more comments button exists at the bottom of the page
+					if ($lastThreadChild.hasClass('morechildren')) {
+
+					 // Click the load more comments button
+					 $lastThreadChild.find('a span').click();
+					}
+				}
+			}, interval);
+
+			console.log('what is enabledstate?' + enabledState);
+
+
+			$('#enabledSwitch').click(function() {
+				console.log("clicked!");
+				$('#enabledLabel').text(enabledState ? 'Enabled' : 'Enable');
+				// Clicking the on/off button sends the opposite value to storage.
+				setEnabledState(!enabledState);
+			});
+
+			// TODO: Upon any change to storage, re-runs scroll code.
+			chrome.storage.onChanged.addListener(function(changes, areaName) {
+					var newState = changes["enabledState"].newValue;
+					console.log(newState);
+					// Update the enabled/disabled button text
+					$('#enabledLabel').text(state ? 'Enabled' : 'Enable');
+			$('#enabledSwitch').attr('checked', state ? 'checked' : null);
+					// Stop or restart the scrolling.
+			});
+
+
 		});
 	});
 }(window.jQuery));
