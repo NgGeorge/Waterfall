4fe91f2d8cdc3f7f9d506326a24efcf242c096de
Eric Chang <eric.chang.cs@gmail.com>
2017-10-22 14:28:08 -0700

    Added logic to redirect to next thread

--- waterfall.js
+++ waterfall.js
@@ -60,42 +60,88 @@
             
             // Only run on reddit
             if (window.location.href.indexOf("reddit") === -1 ) return;
+            if (window.location.href.indexOf("comments") === -1 ) {
+                // Assumes user is on reddit main page or subreddit
+                // TODO: Make this more robust using regex
 
-            // Only match threads that are not mega threads (labeled 1-25)
-            const $queue = $('div.thing').filter( function() {
-                const $rank = $(this).attr('data-rank');
-                if($rank) {
-                    return $rank.match(/[0-9]+/);
+                // Only match threads that are not mega threads (labeled 1-25)
+                const $queue = $('div.thing').filter( function() {
+                    const $rank = $(this).attr('data-rank');
+                    if($rank) {
+                        return $rank.match(/[0-9]+/);
+                    }
+                });
+
+                // Create an array of urls
+                var urls = [];
+                for ( var i = 0; i < $queue.length; i++ ) {
+                    var nextUrl = $queue[i].dataset.permalink;
+                    urls.push(nextUrl);
+                    console.log('Saved');
+                    console.log(nextUrl);
                 }
-            });
 
-            for ( var i = 0; i < $queue.length; i++ ) {
-                const $next = $queue[i].dataset.permalink;
-                console.log($next);
+                // Store urls
+                chrome.storage.local.set({"urls": JSON.parse(JSON.stringify(urls))}, function() {
+                    if (chrome.runtime.error) {
+                        console.log(chrome.runtime.error);
+                    }
+                })
+
+                // Sets next page to be urls[1]
+                chrome.storage.local.set({"index": JSON.stringify(1)}, function() {
+                    if (chrome.runtime.error) {
+                        console.log(chrome.runtime.error);
+                    }
+                });
+            } else { // Assumes user is in a comments page
+
+                // Autoscroll down page 
+                // Interval speed is in milliseconds. 
+                // Will want to hook up an adjustable speed slider 
+                // on the HTML Popup to work with this
+                let interval = 50;
+                var stop = false;
+                const scroll = setInterval(function(){ 
+                window.scrollBy(0, 1);   
+
+                // Check if the current window is at the bottom of the page
+                if ((window.innerHeight + window.scrollY + 1) >= document.body.scrollHeight) {
+
+                    // Gets the last child element of sitetable with the class of thing
+                    const $lastThreadChild = $('.thing:last');
+
+                    // Check if the load more comments button exists at the bottom of the page
+                    if ($lastThreadChild.hasClass('morechildren')) {
+                        // Click the load more comments button
+                        $lastThreadChild.find('a span').click();
+                    } else {
+                        // Check this boolean flag to only redirect once
+                        if (!stop) {
+                            // get next thread to load
+                            chrome.storage.local.get(["index", "urls"], function(storageItems) {
+                                var index = JSON.parse(JSON.stringify(storageItems["index"]));
+                                var urls = JSON.parse(JSON.stringify(storageItems["urls"]));
+
+                                if (index != null && urls != null) {
+                                    // Update the index with the next page
+                                    chrome.storage.local.set({"index": JSON.stringify(1+parseInt(index))});
+                                    console.log(urls[parseInt(index)]);
+                                    if (index < urls.length) {
+                                        window.location.href = urls[parseInt(index)];
+                                    } else {
+                                        console.log("No more results");
+                                    }
+                                } else {
+                                    console.log("No more results");
+                                }
+                            });
+                            stop = true;
+                        }
+                    }
+                }
+                }, interval);
             }
-
-            // Autoscroll down page 
-            // Interval speed is in milliseconds. 
-            // Will want to hook up an adjustable speed slider 
-            // on the HTML Popup to work with this
-            let interval = 50;
-            const scroll = setInterval(function(){ 
-              window.scrollBy(0, 1);   
-
-              // Check if the current window is at the bottom of the page
-              if ((window.innerHeight + window.scrollY + 1) >= document.body.scrollHeight) {
-
-                // Gets the last child element of sitetable with the class of thing
-                const $lastThreadChild = $('.thing:last');
-
-                // Check if the load more comments button exists at the bottom of the page
-                if ($lastThreadChild.hasClass('morechildren')) {
-                 
-                 // Click the load more comments button
-                 $lastThreadChild.find('a span').click();
-                }
-              }
-            }, interval); 
         });
 	});
 }(window.jQuery));
